ee57af92b5ad68d316779db928c366b93849d401
Michal Kowalski <michalkowal571@gmail.com>
2024-04-08 17:31:15 +0200

    plz work

--- app/src/main/java/com/wtfih/heartstitcher/screens/TermsAndConditionsScreen.kt
+++ app/src/main/java/com/wtfih/heartstitcher/screens/TermsAndConditionsScreen.kt
@@ -1,11 +1,14 @@
 package com.wtfih.heartstitcher.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.IntermediateMeasureScope
 import androidx.compose.ui.res.stringResource
@@ -17,19 +20,26 @@ import com.wtfih.heartstitcher.components.NormalTextComponent
 import com.wtfih.heartstitcher.navigation.HeartStitcherRouter
 import com.wtfih.heartstitcher.navigation.Screen
 import com.wtfih.heartstitcher.navigation.SystemBackButtonHandler
+import com.wtfih.heartstitcher.ui.theme.Blue
+import com.wtfih.heartstitcher.ui.theme.Purple
 
 
 @Composable
-fun TermsAndConditionsScreen(){
-    Surface(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White)
-        .padding(16.dp)) {
-        HeadingTextComponent(value = stringResource(id = R.string.terms_and_conditions_heading)
-        )
-    }
-    SystemBackButtonHandler {
-        HeartStitcherRouter.navigateTo(Screen.SignUpScreen)
+fun TermsAndConditionsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.horizontalGradient(listOf(Purple, Blue)))
+            .padding(28.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            HeadingTextComponent(value = stringResource(id = R.string.terms_and_conditions_heading))
+        }
+        SystemBackButtonHandler {
+            HeartStitcherRouter.navigateTo(Screen.SignUpScreen)
+        }
     }
 }
 
